e51377c72900ecf44f88b2bdec5dccdd73d84cfa
ashleygracejefferson7 <ashley@ashleygracejefferson.com>
2025-03-05 16:50:42 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NetworkingMatcher from './NetworkingMatcher';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NetworkingMatcher />
+    </div>
+  );
+}
+
+export default App;
