c09ba16b94050aee1b7e2113ba096d9f3e662803
ashleygracejefferson7 <ashley@ashleygracejefferson.com>
2025-03-09 09:58:11 -0400

    Create RetentionMessage.jsx

---
+++ src/RetentionMessage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RetentionMessage = () => (
+  <div className="mt-4 p-4 bg-yellow-50 border border-yellow-300 rounded">
+    <p className="text-sm text-yellow-800">
+      <strong>Privacy Notice:</strong> Your data will only be kept for the duration of SXSW 2025 and deleted afterward. 
+      We do not share your information with third parties.
+    </p>
+  </div>
+);
+
+export default RetentionMessage;
